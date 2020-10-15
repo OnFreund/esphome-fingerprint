0ab319c6d8f6d30b9eb161bd1410d4e03c9a7ba4
loongyh <loongyh@users.noreply.github.com>
2020-10-15 15:40:28 +0800

    Revert unnecessary changes

--- sensor.py
+++ sensor.py
@@ -8,7 +8,6 @@ from . import CONF_RXXX_ID, RxxxComponent
 DEPENDENCIES = ['rxxx']
 
 CONF_FINGERPRINT_COUNT = "fingerprint_count"
-CONF_STATE = "state"
 CONF_STATUS = "status"
 CONF_CAPACITY = "capacity"
 CONF_SECURITY_LEVEL = "security_level"
@@ -18,7 +17,6 @@ CONF_LAST_CONFIDENCE = "last_confidence"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(CONF_RXXX_ID): cv.use_id(RxxxComponent),
     cv.Optional(CONF_FINGERPRINT_COUNT): sensor.sensor_schema(UNIT_EMPTY, ICON_FINGERPRINT, 0),
-    cv.Optional(CONF_STATE): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0),
     cv.Optional(CONF_STATUS): sensor.sensor_schema(UNIT_EMPTY, ICON_EMPTY, 0),
     cv.Optional(CONF_CAPACITY): sensor.sensor_schema(UNIT_EMPTY, ICON_DATABASE, 0),
     cv.Optional(CONF_SECURITY_LEVEL): sensor.sensor_schema(UNIT_EMPTY, ICON_SECURITY, 0),
@@ -29,8 +27,8 @@ CONFIG_SCHEMA = cv.Schema({
 def to_code(config):
     hub = yield cg.get_variable(config[CONF_RXXX_ID])
 
-    for key in [CONF_FINGERPRINT_COUNT, CONF_STATE, CONF_STATUS, CONF_CAPACITY,
-                CONF_SECURITY_LEVEL, CONF_LAST_FINGER_ID, CONF_LAST_CONFIDENCE]:
+    for key in [CONF_FINGERPRINT_COUNT, CONF_STATUS, CONF_CAPACITY, CONF_SECURITY_LEVEL,
+                CONF_LAST_FINGER_ID, CONF_LAST_CONFIDENCE]:
         if key not in config:
             continue
         conf = config[key]
